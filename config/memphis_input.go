131cb34c6643ba1fd8725dc5b62b746bffebe95e
UnAfraid <unafraid89@gmail.com>
2023-09-15 02:05:00 +0300

    Added headerPrefixes support in memphis

--- config/memphis_input.go
+++ config/memphis_input.go
@@ -5,12 +5,13 @@ import (
 )
 
 type MemphisInput struct {
-	Enabled      bool          `yaml:"enabled"`
-	Hostname     string        `yaml:"hostname"`
-	Username     string        `yaml:"username"`
-	Password     string        `yaml:"password"`
-	Station      string        `yaml:"station"`
-	ConsumerName string        `yaml:"consumerName"`
-	PollInterval time.Duration `yaml:"pollInterval"`
-	BatchSize    int           `yaml:"batchSize"`
+	Enabled        bool              `yaml:"enabled"`
+	Hostname       string            `yaml:"hostname"`
+	Username       string            `yaml:"username"`
+	Password       string            `yaml:"password"`
+	Station        string            `yaml:"station"`
+	ConsumerName   string            `yaml:"consumerName"`
+	PollInterval   time.Duration     `yaml:"pollInterval"`
+	BatchSize      int               `yaml:"batchSize"`
+	HeaderPrefixes map[string]string `yaml:"headerPrefixes"`
 }
