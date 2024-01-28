15805bd7fe0398ae9aba6702b8cdaaa6a86bfd39
UnAfraid <unafraid89@gmail.com>
2024-01-28 19:47:19 +0200

    Added initial prometheus support

--- config/output.go
+++ config/output.go
@@ -3,5 +3,6 @@ package config
 type Output struct {
 	Mqtt       *MqttOutput  `yaml:"mqtt"`
 	InfluxDb2  *InfluxDb2   `yaml:"influxdb2"`
+	Prometheus *Prometheus  `yaml:"prometheus"`
 	Transforms []*Transform `yaml:"transforms"`
 }
