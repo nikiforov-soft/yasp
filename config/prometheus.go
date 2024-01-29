d2e79d8b61638921f2b84b8f756126a7abc91fa8
UnAfraid <unafraid89@gmail.com>
2024-01-30 00:16:46 +0200

    Improved prometheus metrics

--- config/prometheus.go
+++ config/prometheus.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"time"
+)
+
 type Prometheus struct {
 	Enabled        bool                       `yaml:"enabled"`
 	ListenAddr     string                     `yaml:"listenAddr"`
@@ -14,12 +18,26 @@ type PrometheusTlsConfig struct {
 }
 
 type PrometheusMetricsMapping struct {
+	// Counter, Gauge
 	Name        string            `yaml:"name"`
 	Description string            `yaml:"description"`
 	Namespace   string            `yaml:"namespace"`
 	Subsystem   string            `yaml:"subsystem"`
 	Labels      map[string]string `yaml:"labels"`
-	Type        string            `yaml:"type"`
-	Value       string            `yaml:"value"`
-	Condition   string            `yaml:"condition"`
+	// Histogram
+	Buckets                         []float64     `yaml:"buckets"`
+	NativeHistogramBucketFactor     float64       `yaml:"nativeHistogramBucketFactor"`
+	NativeHistogramZeroThreshold    float64       `yaml:"nativeHistogramZeroThreshold"`
+	NativeHistogramMaxBucketNumber  uint32        `yaml:"nativeHistogramMaxBucketNumber"`
+	NativeHistogramMinResetDuration time.Duration `yaml:"nativeHistogramMinResetDuration"`
+	NativeHistogramMaxZeroThreshold float64       `yaml:"nativeHistogramMaxZeroThreshold"`
+	// Summary
+	Objectives map[float64]float64 `yaml:"objectives"`
+	MaxAge     time.Duration       `yaml:"maxAge"`
+	AgeBuckets uint32              `yaml:"ageBuckets"`
+	BufCap     uint32              `yaml:"bufCap"`
+	// Additional context
+	Type      string `yaml:"type"`
+	Value     string `yaml:"value"`
+	Condition string `yaml:"condition"`
 }
